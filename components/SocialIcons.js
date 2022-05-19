c310dee49400032fa511498e30caaedcd1aaf4e6
Lillian Wolfson <lillywolf@gmail.com>
2022-05-18 23:40:39 -0400

    small fix

--- components/SocialIcons.js
+++ components/SocialIcons.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function SocialIcons() {
     return (
-        <div className='relative block h-8'>
+        <div className='flex block h-8'>
             {/* <div className='relative inline-block h-7 w-7 mr-3'>
                 <a href="http://twitter.com/silobrooklyn"><Image src='/twitter.svg' layout='fill'/></a>
             </div> */}
