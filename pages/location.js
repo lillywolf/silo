81836be4cc03575c134ebf322b6be5f6dd1bb3d7
Lillian Wolfson <lillywolf@gmail.com>
2022-08-20 11:42:06 -0400

    fix location page

--- pages/location.js
+++ pages/location.js
@@ -4,12 +4,12 @@ export default function Location() {
     return (
         <>
             <div className='absolute w-full text-left'>
-                <div className='flex mt-40 w-9/12 text-s mx-auto justify-around'>
-                    <div className='relative mt-4'>
+                <div className='flex mt-40 mx-6 flex-col md:flex-row md:justify-around md:mx-auto md:w-9/12'>
+                    <div className='relative mt-4 mb-4 text-s text-center md:text-left'>
                         <p>SUBWAY</p>
                         <p>L Train (Jefferson Stop)</p>
                     </div>
-                    <div className='relative w-[600px] h-[400px]'>
+                    <div className='relative md:w-8/12'>
                         <Image src='/map.png' width={600} height={400} layout="responsive" objectFit="contain"/>
                     </div>
                 </div>
