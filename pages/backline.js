a8751189f7996bc706e59ef3f273c19319f57668
Lillian Wolfson <lillywolf@gmail.com>
2022-08-24 18:30:52 -0400

    add backline page

---
+++ pages/backline.js
@@ -0,0 +1,73 @@
+export default function Backline() {
+    return (
+        <>
+            <div className='absolute w-full text-left'>
+                <div className='relative mt-40 mb-40 w-9/12 text-s mx-auto lg:w-6/12'>
+                    <p className='text-2xl text-white'>BACKLINE</p>
+                    <br></br>
+                    <p className='text-white'>MAIN ROOM SOUND SYSTEM</p>
+                    <br></br>
+                    <p>2 x Danley SH96HO mains</p>
+                    <p>2 x Danley BC218 subwoofers</p>
+                    <p>2 x Danley SH46 rear fills (low-end extension down to 22 Hz)</p>
+                    <p>2 x Danley 20K4 amplifiers (total 40,000 watts amplifier power)</p>
+                    <p>2 x Electrovoice PXM-12MP stage monitors configurable on stands facing DJ console or as floor wedges</p>
+                    <p>Allen & Heath SQ-5 digital mixer (48 channels, 96kHz processing) with AR2412 digital stage box</p>
+                    <br></br>
+                    <p className='text-white'>DJ</p>
+                    <br></br>
+                    <p>2 x Pioneer CDJ-3000</p>
+                    <p>2 x Pioneer PLX-1000 turntables</p>
+                    <p>Pioneer DJM-V10 6-channel mixer</p>
+                    <br></br>
+                    <p className='text-white'>FRONT ROOM (SMALL PA)</p>
+                    <br></br>
+                    <p>2 x Electro-Voice ELX200-12P 12" 1200W 2-Way powered loudspeakers</p>
+                    <p>3000 Watt Behringer Eurolive B1800XP subwoofer</p>
+                    <p>2 Yamaha wedge stage monitors</p>
+                    <p>Mackie ProFX16 Mixer w/ reverb and delay</p>
+                    <br></br>
+                    <p className='text-white'>MICS</p>
+                    <br></br>
+                    <p>2 x Shure BLX288PG58 wireless mics</p>
+                    <p>3 x Shure SM-58 vocal mic</p>
+                    <p>1 x Shure SM-57 instrument mic</p>
+                    <p>6 x Behringer XM8500 super cardioid vocal mics</p>
+                    <p>1 x Audix D6 kick drum mic</p>
+                    <p>5 mic stands, 1 kick mic stand, two keyboard stands, music stand</p>
+                    <br></br>
+                    <p className='text-white'>INSTRUMENT AMPLIFIERS</p>
+                    <br></br>
+                    <p>200 watt Yamaha keyboard amp w/ 4 inputs</p>
+                    <p>150 watt Ampeg bass amp</p>
+                    <p>100 watt Fender Champion Combo amp with effects</p>
+                    <p>2 DI boxes for plugging acoustic guitars/violins directly into the PA</p>
+                    <br></br>
+                    <p className='text-white'>INSTRUMENTS</p>
+                    <br></br>
+                    <p>88-key weighted Yamaha Motif keyboard</p>
+                    <p>PDP Centerstage 5-piece drum kit. Drummers must bring snare, cymbals, and additional hardware if they need more than a hi-hat and two cymbal stands.</p>
+                    <p>Pioneer XDJ-RX2 DJ Controller (compatible with Rekordbox)</p>
+                    <br></br>
+                    <p className='text-white'>LIGHTING</p>
+                    <br></br>
+                    <p>14 x Rogue 300W R3 Beam Lights</p>
+                    <p>4 x ADJ 18p Hex Par Stage Wash (capable of full blacklight, amber, white)</p>
+                    <p>3 x ADJ Vizi Hex Wash 7 - Moving Head Stage Wash (capable of full blacklight, amber, white)</p>
+                    <p>2 x Chauvet Hurricane Hazer 1DX</p>
+                    <p>2 x ADJ Ultra Hex Bar uplighting (capable of full blacklight, amber, white)</p>
+                    <p>5 x ADJ 6p Hex Par - Fill Washes</p>
+                    <p>Motorized Disco Ball with multiple color-programmable pin spots</p>
+                    <p>Chamsys QuickQ30 Control Board</p>
+                    <br></br>
+                    <p className='text-white'>PROJECTION SYSTEM</p>
+                    <br></br>
+                    <p>NEC PH1202HL1 12,000 lumen large venue DLP laser projector</p>
+                    <p>8 x 4000 lumen, 300,000:1 contrast Optoma Projectors, projection mapped across ceiling</p>
+                    <p>12-core liquid cooled PC workstation with 18x4K HDMI outputs, running Resolume Arena 7</p>
+                    <br></br>
+                </div>
+            </div>
+        </>
+    );
+}
