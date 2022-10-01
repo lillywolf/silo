2d8c6c284839997803848cfca908e27a9a958a62
Lillian Wolfson <lillywolf@gmail.com>
2022-10-01 15:53:26 -0400

    fix syntax

--- components/GTM.js
+++ components/GTM.js
@@ -1,13 +1,17 @@
+function setup() {
+    window.dataLayer = window.dataLayer || []
+    function gtag(){dataLayer.push(arguments)}
+    gtag('js', new Date());
+    
+    gtag('config', 'G-RS8GPCPFCR');
+}
+
 export default GTM = () => {
     return (
         <>
             <script async src="https://www.googletagmanager.com/gtag/js?id=G-RS8GPCPFCR"></script>
             <script>
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments)}
-                gtag('js', new Date());
-                
-                gtag('config', 'G-RS8GPCPFCR');
+                {setup()}
             </script>
         </>
     );
