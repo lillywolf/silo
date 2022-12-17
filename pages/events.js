a75a7972b19eae67f0221c352837c4f8191f6cd7
Lillian Wolfson <lillywolf@gmail.com>
2022-12-16 20:20:41 -0500

    fix events widget

--- pages/events.js
+++ pages/events.js
@@ -2,12 +2,12 @@ import { useEffect } from "react";
 
 export default function Events() {
     useEffect(() => {
-        window.DiceEventListWidget.create({"information":"simple","eventTitle":"event","showImages":true,"showAudio":true,"showNewBadge":false,"hidePostponed":false,"hideCancelled":true,"layout":"gallery","roundButtons":true,"theme":"dark","fontFamily":"inherit","partnerId":"c3d5496a","apiKey":"0fmc87mV4m1WZHIUtCtN7GDlM0b2_Pqx4JsFZARHFUgIb0yY","version":2,"showPrice":true,"__p":false,"__pc":{},"highlightColour":"#fb00ff","numberOfEvents":"10"});
-    }, []);
-
+        window.DiceEventListWidget.create({"information":"simple","eventTitle":"event","showImages":true,"showAudio":true,"showNewBadge":false,"hidePostponed":false,"hideCancelled":true,"layout":"gallery","roundButtons":true,"theme":"dark","fontFamily":"inherit","partnerId":"c3d5496a","apiKey":"nkvo0ySBVc1OdE6tLTHO35ZF35vioOQV2chuvMob","version":2,"showPrice":true,"__p":false,"__pc":{},"highlightColour":"#fb00ff","numberOfEvents":"10","venues":["Silo Brooklyn"]})
+    })   
+ 
     return (
-        <div className='absolute text-center text-white w-full mt-40 mx-auto'>
-            <div id="dice-event-list-widget"></div>
+        <div className='absolute text-center text-white w-full mt-40'>
+            <div className='mx-auto w-9/12' id="dice-event-list-widget"></div>
             <script src="https://widgets.dice.fm/dice-event-list-widget.js" type="text/javascript"></script>
         </div>
     );
