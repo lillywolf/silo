f4a90d0e5c07790987b2194c914e9706c75d715b
Lillian Wolfson <lillywolf@gmail.com>
2022-12-16 20:32:08 -0500

    events on home page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import EmailSignup from '../components/EmailSignup'
 import Consent from '../components/Consent'
+import { useEffect } from 'react'
 
 export default function Home() {
+  useEffect(() => {
+    window.DiceEventListWidget.create({"information":"simple","eventTitle":"event","showImages":true,"showAudio":true,"showNewBadge":false,"hidePostponed":false,"hideCancelled":true,"layout":"gallery","roundButtons":true,"theme":"dark","fontFamily":"inherit","partnerId":"c3d5496a","apiKey":"nkvo0ySBVc1OdE6tLTHO35ZF35vioOQV2chuvMob","version":2,"showPrice":true,"__p":false,"__pc":{},"highlightColour":"#fb00ff","numberOfEvents":"10","venues":["Silo Brooklyn"]})
+    })   
+
   return (
     <div className={styles.container}>
       <Head>
@@ -17,19 +21,11 @@ export default function Home() {
 
       <main className={ `${ styles.main } m-auto relative` }>
         <div className='relative'>
-          <div className='px-4 sm:w-[700px] mx-auto mt-16 md:mt-40'>
-            <Image
-              src="/exterior-compressed.jpg"
-              width="90%"
-              height="57%"
-              layout="responsive"
-              objectFit="contain"
-              alt="Photo of the venue exterior"
-            />
-          </div>
-          <div className='m-auto pt-6 sm:w-[520px] lg:w-[600px]'>
+        <div className='mx-auto w-9/12 pt-40' id="dice-event-list-widget"></div>
+        <script src="https://widgets.dice.fm/dice-event-list-widget.js" type="text/javascript"></script>
+        <div className='m-auto sm:w-[520px] lg:w-[600px]'>
             <EmailSignup />
-          </div>
+        </div>
         </div>
       </main>
     </div>
