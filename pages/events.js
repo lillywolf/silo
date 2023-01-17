88004c84c5b4fe56744441dbb55e6d61737fdaca
Lillian Wolfson <lillywolf@gmail.com>
2023-01-17 17:47:29 -0500

    fix issue with consent and dice listings

--- pages/events.js
+++ pages/events.js
@@ -2,9 +2,11 @@ import { useEffect } from "react";
 
 export default function Events() {
     useEffect(() => {
-        window.DiceEventListWidget.create({"information":"simple","eventTitle":"event","showImages":true,"showAudio":true,"showNewBadge":false,"hidePostponed":false,"hideCancelled":true,"layout":"gallery","roundButtons":true,"theme":"dark","fontFamily":"inherit","partnerId":"c3d5496a","apiKey":"nkvo0ySBVc1OdE6tLTHO35ZF35vioOQV2chuvMob","version":2,"showPrice":true,"__p":false,"__pc":{},"highlightColour":"#fb00ff","numberOfEvents":"10","venues":["Silo Brooklyn"]})
+        if (window.DiceEventListWidget) {
+            window.DiceEventListWidget.create({"information":"simple","eventTitle":"event","showImages":true,"showAudio":true,"showNewBadge":false,"hidePostponed":false,"hideCancelled":true,"layout":"gallery","roundButtons":true,"theme":"dark","fontFamily":"inherit","partnerId":"c3d5496a","apiKey":"nkvo0ySBVc1OdE6tLTHO35ZF35vioOQV2chuvMob","version":2,"showPrice":true,"__p":false,"__pc":{},"highlightColour":"#fb00ff","numberOfEvents":"10","venues":["Silo Brooklyn"]})
     
-        return () => window.DiceEventListWidget.destroy();
+            return () => window.DiceEventListWidget.destroy();
+        }
     }, [])   
  
     return (
