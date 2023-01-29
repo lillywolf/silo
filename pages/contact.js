b192a55a5ed6ed047231ce681451ae7e399e076a
Lillian Wolfson <lillywolf@gmail.com>
2023-01-29 16:56:17 -0500

    fix dice widget?

--- pages/contact.js
+++ pages/contact.js
@@ -10,9 +10,9 @@ export default function Contact() {
                 <div className='relative mt-16 lg:mt-28'>
                     <p>for booking inquiries:</p>
                     <a href="mailto:booking@silobrooklyn.com" className='underline'>booking@silobrooklyn.com</a>
-                    <p>for lost and found:</p>
+                    <p className='mt-4'>for lost and found:</p>
                     <a href="mailto:lostandfound@silobrooklyn.com" className='underline'>lostandfound@silobrooklyn.com</a>
-                    <p>for accessibility requests:</p>
+                    <p className='mt-4'>for accessibility requests:</p>
                     <a href="mailto:a11y@silobrooklyn.com" className='underline'>a11y@silobrooklyn.com</a>
                     <p className='mt-4'>for all others:</p>
                     <a href="mailto:contact@silobrooklyn.com" className='underline'>contact@silobrooklyn.com</a>
