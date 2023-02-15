957c758cef3499de87d4cc4304bc785b8b4ac367
Alex Neuhausen <alexneu@gmail.com>
2023-02-14 20:50:25 -0500

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -18,21 +18,6 @@ export default function Contact() {
                     <a href="mailto:a11y@silobrooklyn.com" className='underline'>a11y@silobrooklyn.com</a>
                     <p className='mt-4'>for all others:</p>
                     <a href="mailto:contact@silobrooklyn.com" className='underline'>contact@silobrooklyn.com</a>
-                    {/* <br></br>
-                    <br></br>
-                    <br></br>
-                    <p>BACKLINE</p>
-                    <br></br>
-                    <p>Sound:</p>
-                    <p>2x Danley BC218 subwoofers</p>
-                    <p>2x Danley SH96HO mains</p>
-                    <p>2x Danley SH46 rear fills</p>
-                    <p>2x DNA 20K4 PRO amplifier</p>
-                    <br></br>
-                    <p>DJ:</p>
-                    <p>4x CDJ 3000s</p>
-                    <p>Pioneer DJM-V10</p>
-                    <p>2x Technics 1200 turntables</p> */}
                 </div>
             </div>
         </>
