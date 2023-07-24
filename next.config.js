3e2d3c67e17164bae7cf5ad6be7466f315ced361
Lillian Wolfson <lillywolf@gmail.com>
2023-07-24 07:31:52 -0400

    [LW] remove /photos and /videos, access from s3

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["dice-media.imgix.net"]
+    domains: ["dice-media.imgix.net", "silobrooklyn.s3.us-east-2.amazonaws.com"]
   },
   async redirects() {
     return [
--- pages/photos.js
+++ pages/photos.js
@@ -1,30 +1,43 @@
 import Image from 'next/image';
-import { useState } from 'react';
-import path from 'path';
+import { parseStringPromise } from 'xml2js';
 
-var fs = require('fs');
+const S3_URL = 'https://silobrooklyn.s3.us-east-2.amazonaws.com';
+
+// If we ever want to access the S3 bucket with auth, we'll need to create a signed request. Use crypto lib and 
+// Refer to https://docs.aws.amazon.com/AmazonS3/latest/userguide/RESTAuthentication.html
+// const CanonicalizedResource = `/silobrooklyn/photos/`;
+// const StringToSign = `HTTP-Verb\nContent-MD5\nContent-Type\nDate\n${ CanonicalizedAmzHeaders }${ CanonicalizedResource };`
+// crypto.createHmac('sha1', StringToSign)
+//     .update(process.env.AWS_SECRET)
+// var files = fs.readdirSync(dir);
 
 export async function getServerSideProps(context) {
-    const dirRelativeToPublicFolder = 'assets/photos/'
-    const dir = path.resolve('./public', dirRelativeToPublicFolder);
-    var files = fs.readdirSync(dir);
+    const bucketMetadata = await fetch(S3_URL);
+    const text = await bucketMetadata.text();
+    const xml = await parseStringPromise(text);
+
+    const urls = xml.ListBucketResult.Contents.map((obj) => {
+        if ((/(^photos.*)/g).exec(obj.Key[0])) {
+            return obj.Key[0];
+        }
+    }).filter(url => url);
 
     return {
         props: {
-            files
+            urls
         }
     }
 }
 
-export default function Photos({ files }) {
+export default function Photos({ urls }) {
     return (
       <>
         <div className="photos">
             {
-                files.map(file => {
+                urls.map(url => {
                     return (
-                        <div className="photo" key={file}>
-                            <Image width={200} height={200} src={ `/assets/photos/${ file }` } objectFit="cover" />
+                        <div className="photo" key={url}>
+                            <Image width={200} height={200} src={ `${ S3_URL }/${ url }` } objectFit="cover" />
                         </div>
                     );
                 })
