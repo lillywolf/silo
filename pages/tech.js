d7bb2ef711c465756ffba6df276a5678735e8985
Alex Neuhausen <alexneu@gmail.com>
2025-10-14 14:58:29 +0200

    Update tech.js

--- pages/tech.js
+++ pages/tech.js
@@ -77,6 +77,7 @@ export default function Tech() {
                     <p>24 x Rogue 300W R3 Beam Lights</p>
                     <p>4 x ADJ 18p Hex Par Stage Wash (capable of full blacklight, amber, white)</p>
                     <p>5 x ADJ Vizi Hex Wash 7 - Moving Head Stage Wash (capable of full blacklight, amber, white)</p>
+                    <p>4 x X-Laser Skywriter HPX M-20 RGB lasers</p>
                     <p>4 x X-Laser Skywriter HPX M-5 RGB lasers</p>
                     <p>8 x ADJ 12p Hex Par Blinders</p>
                     <p>4 x ADJ Jolt Panel FX2 Wash/Blinder/Strobe Fixtures</p>
@@ -85,7 +86,8 @@ export default function Tech() {
                     <p>8 x ADJ Inno Spot Pro Beam Lights (front room)</p>
                     <p>1 x Chauvet DJ Hurricane 2000 Fog Machine</p>
                     <p>1 x Chauvet DJ Hurricane Hazer 1DX (front room)</p>
-                    <p>PC with three touch screens and GrandMA 2 Command Wing and 2 X Fader Wing Control Boards</p>
+                    <p>PC with three touch screens, GrandMA 2 Command Wing and 2 X Fader Wing Control Boards</p>
+                    <p>Windows laptop with Pangolin Quickshow and an Akai APC Mini MK2 Controller for laser control</p>
                     <br></br>
                     <p className='text-white'>PROJECTION SYSTEM</p>
                     <br></br>
